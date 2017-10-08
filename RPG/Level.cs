2aef08bd8712412159d5d96503d4bab00f316c88
Mads Mogensen <mads256c@users.noreply.github.com>
2017-10-08 22:01:09 +0200

    Removed some using directives

--- RPG/Level.cs
+++ RPG/Level.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using RPG.Objects;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace RPG
